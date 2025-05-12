ccf3fb8051f120a041ab2b1bc262f5b65501f5b7
pr3o <158464877+pr3o@users.noreply.github.com>
2025-05-12 10:54:44 +0200

    COnversione primi 3 numeri da intero a romano close #2

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -5,14 +5,21 @@
 package it.unipd.mtss;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IntegerToRomanTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testFirstThreeNumbers() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+
+    @Test
+    public void testNumberOutOfBounds() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(0));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(5));
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(-1));
     }
 }
