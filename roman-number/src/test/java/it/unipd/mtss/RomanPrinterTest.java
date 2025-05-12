5086bb13c10871fee9f2809377dff805f810b7d0
Filippo Compagno <filippo.compag@gmail.com>
2025-05-12 10:40:25 +0200

    Setup progetto con Checkstyle e file di base

---
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// Filippo Compagno 2101082
+// Leonardo Preo 2101092
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RomanPrinterTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
